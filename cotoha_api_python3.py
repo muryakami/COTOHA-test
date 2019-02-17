dd94afc8056e057e05d283868a156ae53e96cf24
YukiMurakami <y-murakami@nekojarashi.com>
2019-02-17 22:42:24 +0900

    サンプルの追加

--- cotoha_api_python3.py
+++ cotoha_api_python3.py
@@ -1,10 +1,7 @@
 # -*- coding:utf-8 -*-
 
-import os
-import urllib.request
 import json
-import configparser
-import codecs
+import urllib.request
 
 
 # COTOHA API操作用クラス
@@ -345,31 +342,3 @@ class CotohaApi:
         res_body = json.loads(res_body)
         # レスポンスボディから解析結果を取得
         return res_body
-
-
-if __name__ == '__main__':
-    # ソースファイルの場所取得
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__)) + "/"
-
-    # 設定値取得
-    config = configparser.ConfigParser()
-    config.read(APP_ROOT + "config.ini")
-    CLIENT_ID = config.get("COTOHA API", "Developer Client id")
-    CLIENT_SECRET = config.get("COTOHA API", "Developer Client secret")
-    DEVELOPER_API_BASE_URL = config.get("COTOHA API", "Developer API Base URL")
-    ACCESS_TOKEN_PUBLISH_URL = config.get(
-        "COTOHA API", "Access Token Publish URL")
-
-    # COTOHA APIインスタンス生成
-    cotoha_api = CotohaApi(CLIENT_ID, CLIENT_SECRET,
-                           DEVELOPER_API_BASE_URL, ACCESS_TOKEN_PUBLISH_URL)
-
-    # 解析対象文
-    sentence = "すもももももももものうち"
-
-    # 構文解析API実行
-    result = cotoha_api.parse(sentence)
-
-    # 出力結果を見やすく整形
-    result_formated = json.dumps(result, indent=4, separators=(',', ': '))
-    print(codecs.decode(result_formated, 'unicode-escape'))
