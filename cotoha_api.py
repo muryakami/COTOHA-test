0997b0af71bb42516184bcdc3e03a4bb9233ff53
Yuki Murakami <y-murakami@nekojarashi.com>
2019-03-19 01:42:07 +0900

    cotoha_api のリファクタリング

--- cotoha_api.py
+++ cotoha_api.py
@@ -32,12 +32,10 @@ class CotohaApi:
     def getAccessToken(self):
         # アクセストークン取得URL指定
         url = self.access_token_publish_url
-
         # ヘッダ指定
         headers = {
             "Content-Type": "application/json;charset=UTF-8"
         }
-
         # リクエストボディ指定
         data = {
             "grantType": "client_credentials",
@@ -46,289 +44,114 @@ class CotohaApi:
         }
         # リクエストボディ指定をJSONにエンコード
         data = json.dumps(data).encode()
-
         # リクエスト生成
         req = urllib.request.Request(url, data, headers)
-
         # リクエストを送信し、レスポンスを受信
         res = urllib.request.urlopen(req)
-
         # レスポンスボディ取得
         res_body = res.read()
-
         # レスポンスボディをJSONからデコード
         res_body = json.loads(res_body)
-
         # レスポンスボディからアクセストークンを取得
         self.access_token = res_body["access_token"]
 
     # 構文解析API
-
     def parse(self, sentence):
-        # 構文解析API URL指定
+        # URL指定
         url = self.developer_api_base_url + "v1/parse"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "sentence": sentence
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
-
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+        return self.__apiHelper__(url, data)
 
     # 固有表現抽出API
-
     def ne(self, sentence):
-        # 固有表現抽出API URL指定
+        # URL指定
         url = self.developer_api_base_url + "v1/ne"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "sentence": sentence
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
-
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+        return self.__apiHelper__(url, data)
 
     # 照応解析API
-
     def coreference(self, document):
-            # 照応解析API 取得URL指定
+        # URL指定
         url = self.developer_api_base_url + "beta/coreference"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "document": document
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
-
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+        return self.__apiHelper__(url, data)
 
     # キーワード抽出API
-
     def keyword(self, document):
-        # キーワード抽出API URL指定
+        # URL指定
         url = self.developer_api_base_url + "v1/keyword"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "document": document
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
-
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+        return self.__apiHelper__(url, data)
 
     # 類似度算出API
-
     def similarity(self, s1, s2):
         # 類似度算出API URL指定
         url = self.developer_api_base_url + "v1/similarity"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "s1": s1,
             "s2": s2
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
-
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+        return self.__apiHelper__(url, data)
 
     # 文タイプ判定API
-
     def sentenceType(self, sentence):
-        # 文タイプ判定API URL指定
+        # URL指定
         url = self.developer_api_base_url + "v1/sentence_type"
-        # ヘッダ指定
-        headers = {
-            "Authorization": "Bearer " + self.access_token,
-            "Content-Type": "application/json;charset=UTF-8",
-        }
         # リクエストボディ指定
         data = {
             "sentence": sentence
         }
-        # リクエストボディ指定をJSONにエンコード
-        data = json.dumps(data).encode()
-        # リクエスト生成
-        req = urllib.request.Request(url, data, headers)
         # リクエストを送信し、レスポンスを受信
-        try:
-            res = urllib.request.urlopen(req)
-        # リクエストでエラーが発生した場合の処理
-        except urllib.request.HTTPError as e:
-            # ステータスコードが401 Unauthorizedならアクセストークンを取得し直して再リクエスト
-            if e.code == 401:
-                print("get access token")
-                self.access_token = getAccessToken(
-                    self.client_id, self.client_secret)
-                headers["Authorization"] = "Bearer " + self.access_token
-                req = urllib.request.Request(url, data, headers)
-                res = urllib.request.urlopen(req)
-            # 401以外のエラーなら原因を表示
-            else:
-                print("<Error> " + e.reason)
+        return self.__apiHelper__(url, data)
 
-        # レスポンスボディ取得
-        res_body = res.read()
-        # レスポンスボディをJSONからデコード
-        res_body = json.loads(res_body)
-        # レスポンスボディから解析結果を取得
-        return res_body
+    # 構文解析API
+    def sentiment(self, sentence):
+        # URL指定
+        url = self.developer_api_base_url + "v1/sentiment"
+        # リクエストボディ指定
+        data = {
+            "sentence": sentence
+        }
+        # リクエストを送信し、レスポンスを受信
+        return self.__apiHelper__(url, data)
 
     # ユーザ属性推定API
-
     def userAttribute(self, document):
-        # ユーザ属性推定API URL指定
+        # URL指定
         url = self.developer_api_base_url + "beta/user_attribute"
+        # リクエストボディ指定
+        data = {
+            "document": document
+        }
+        # リクエストを送信し、レスポンスを受信
+        return self.__apiHelper__(url, data)
+
+    # API ヘルパー
+    def __apiHelper__(self, url, data):
         # ヘッダ指定
         headers = {
             "Authorization": "Bearer " + self.access_token,
             "Content-Type": "application/json;charset=UTF-8",
         }
-        # リクエストボディ指定
-        data = {
-            "document": document
-        }
+
         # リクエストボディ指定をJSONにエンコード
         data = json.dumps(data).encode()
         # リクエスト生成
